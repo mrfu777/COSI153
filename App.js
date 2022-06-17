5ca51b195c8761f90e6abdbb7750286358ff2af8
unknown <mrfu@brandeis.edu>
2022-06-17 18:16:11 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
 
-export default function App() {
+const BMI = () => {
+  const [y, setY] = useState("");
+  const [x, setX] = useState("");
+  const [z, setZ] = useState("");
+  const [bmi,setBMI] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 10}}>
+      <Text style={{fontSize: 30,fontWeight: 'bold'}}>
+        Distance of (x,y,z) from (0,0,0)
+      </Text>
+      <Text>
+        Write the code for this app which calculates
+        d = Math.sqrt(x*x+y*y+z*z)
+      </Text>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Enter your x"
+        onChangeText={newText => 
+             setX(newText)}
+        defaultValue={x}
+      />
+     <TextInput
+        style={{weight: 40}}
+        placeholder="Enter your y"
+        onChangeText={newText => 
+             setY(newText)}
+        defaultValue={y}
+      />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Enter your z"
+        onChangeText={newText => 
+             setZ(newText)}
+        defaultValue={z}
+      />
+      <Button
+        title="calculate distance"
+        onPress = {() => {
+            setBMI(Math.sqrt(x*x+y*y+z*z))
+        }}
+      />
+      <Text>
+        distance to ({x},{y},{z}) is d = {bmi}
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default BMI;
