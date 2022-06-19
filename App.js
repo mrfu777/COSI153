34efdd04a4c605a300f6368c15122a3b6afd1098
unknown <mrfu@brandeis.edu>
2022-06-19 16:08:43 -0400

    fu1

--- App.js
+++ App.js
@@ -1,55 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, FlatList, View,Button, Image, StyleSheet, ActivityIndicator, TextInput } from 'react-native';
 
-const BMI = () => {
-  const [y, setY] = useState("");
-  const [x, setX] = useState("");
-  const [z, setZ] = useState("");
-  const [bmi,setBMI] = useState(0);
 
-  return (
-    <View style={{padding: 10}}>
-      <Text style={{fontSize: 30,fontWeight: 'bold'}}>
-        Distance of (x,y,z) from (0,0,0)
-      </Text>
-      <Text>
-        Write the code for this app which calculates
-        d = Math.sqrt(x*x+y*y+z*z)
-      </Text>
-      <TextInput
-        style={{height: 40}}
-        placeholder="Enter your x"
-        onChangeText={newText => 
-             setX(newText)}
-        defaultValue={x}
-      />
-     <TextInput
-        style={{weight: 40}}
-        placeholder="Enter your y"
-        onChangeText={newText => 
-             setY(newText)}
-        defaultValue={y}
-      />
-      <TextInput
-        style={{height: 40}}
-        placeholder="Enter your z"
-        onChangeText={newText => 
-             setZ(newText)}
-        defaultValue={z}
-      />
-      <Button
-        title="calculate distance"
-        onPress = {() => {
-            setBMI(Math.sqrt(x*x+y*y+z*z))
-        }}
-      />
-      <Text>
-        distance to ({x},{y},{z}) is d = {bmi}
-      </Text>
-    </View>
-  );
+
+const CPA02 = () => {
+    const [data,setData] = useState([]);
+    const [loading,setLoading] = useState(true);
+    const [ingredient, setIngredient] = useState('beef')
+
+    const getMeals = async () => {
+        try {
+          const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+          const response = await fetch(url);
+          const json = await response.json();
+          setData(json.meals); 
+        } catch (error) {
+          console.error(error);
+        } finally {
+            setLoading(false);
+        }
+      };
+
+    useEffect(() => {getMeals()}, [ingredient])
+    const renderItem = ({ item }) => (
+      <Item title1={item.strMeal} url={item.strMealThumb} id1={item.idMeal}/>
+    );
+    return(
+        <View style={{ flex: 1, padding: 24 }}>
+          <Text style={styles.title}>Meal Finder</Text>
+          <TextInput
+            placeholder="Enter Ingredient"
+            onChangeText={newText => 
+              setIngredient(newText)}
+          />
+          <Button
+                title="Submit"
+                onPress={() => {
+                    setIngredient(newText)
+                }}
+            />
+        
+          {loading ? <ActivityIndicator/> : (
+            <FlatList
+              data={data}
+              keyExtractor={({ id }, index) => id}
+              renderItem={renderItem}
+            />
+          )}
+        
+        </View>
+    );
 }
 
-export default BMI;
-//aaaaaaaaaa
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  item: {
+    backgroundColor: 'red',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: "row-reverse"
+  },
+  image: {
+    width: 66,
+    height: 58,
+    left: 150,
+  },
+  text: {
+    fontSize: 15,
+  }
+});
+
+const Item = ({ title1, url, id1 }) => (
+  <View style={styles.item}>
+    <Text style={styles.text}>{title1}</Text>
+    <View style={{ flex: 1}}>
+      <Image style={styles.image} source={{uri:url}} />
+    </View>
+    <Text style={styles.text}>{id1}</Text>
+  </View>
+);
+
+export default CPA02;
